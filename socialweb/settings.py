f37c3860f60eb95d32f6ac22edb583ac259bdea3
vikasminj <vikashminj1999@gmail.com>
2021-07-29 13:28:27 +0530

    settingupdateonce

--- socialweb/settings.py
+++ socialweb/settings.py
@@ -110,8 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/UploadImage/'
-MEDIA_ROOT = BASE_DIR / 'UploadImage'
+# MEDIA_URL = '/UploadImage/'
+# MEDIA_ROOT = BASE_DIR / 'UploadImage'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
